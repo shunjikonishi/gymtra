45a308093f27ce6dd49b06a684836747154c7702
Shunji Konishi <shun.konishi@nifty.com>
2013-07-09 21:19:43 +0900

    Implementing s3 upload

--- app/controllers/Videos.scala
+++ app/controllers/Videos.scala
@@ -1,14 +1,35 @@
 package controllers
 
 import java.net.URLEncoder;
+import java.io.File;
 import scala.concurrent.ExecutionContext.Implicits.global;
 import play.api.mvc.Controller;
 import play.api.mvc.Action;
 import play.api.mvc.RequestHeader;
 import play.api.libs.ws.WS;
+import play.api.libs.json.Json;
 
-import models.FacebookManager;
+import models.AWSManager;
+import models.AWSManager.uploadInfoFormat;
+
+import jp.co.flect.play2.utils.Params;
 
 object Videos extends Controller {
 	
+	val man = AWSManager.apply("fullin-fullout");
+
+	private def redirectUri(implicit request: RequestHeader) = {
+		"http://" + request.host + "/main";
+	}
+
+	def prepareUpload = Action { implicit request =>
+		val filename = Params(request).get("filename");
+		filename match {
+			case Some(s) =>
+println("filename: " + s + ", " + new File(s).getName);
+				Ok(Json.toJson(man.prepare(new File(s).getName, redirectUri)));
+			case None =>
+				BadRequest;
+		}
+	}
 }
--- app/models/AWSManager.scala
+++ app/models/AWSManager.scala
@@ -3,6 +3,10 @@ package models;
 import java.util.UUID;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import org.apache.commons.codec.binary.Base64;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import play.api.libs.json.Json;
 
 case class UploadInfo(
 	filename: String,
@@ -20,25 +24,33 @@ object AWSManager {
 	
 	val POLICY = """{"expiration" : "%s","conditions":[
 {"bucket":"%s"},
-{"starts-with","$key","videos/"},
+["starts-with","$key","videos/"],
 {"acl":"private"},
 {"success_action_redirect":"%s"},
 ["starts-with","$Content-Type",""],
 ["content-length-range",0,1048576000]]}""";
 
+	def apply(bucket: String) = new AWSManager(ACCESS_KEY, SECRET_KEY, bucket);
+	implicit val uploadInfoFormat = Json.format[UploadInfo];
 }
 
 class AWSManager(accessKey: String, secretKey: String, bucket: String) {
 
 	import AWSManager.POLICY;
-	
-	def prepare(filename: String, redirectUri: String): UploadInfo = {
-		val key = UUID.randomUUID;
+
+	private def generatePolicy(filename: String, redirectUri: String) = {
 		val policy = POLICY.format(
 			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(new Date(System.currentTimeMillis + 60 * 60 * 1000)),
 			bucket,
 			redirectUri
 		);
+println("policy: " + policy);
+		new String(Base64.encodeBase64(policy.getBytes("utf-8")), "utf-8");
+	}
+	
+	def prepare(filename: String, redirectUri: String): UploadInfo = {
+		val key = UUID.randomUUID;
+		val policy = generatePolicy(filename, redirectUri);
 		val signature = sign(policy);
 		val ext = filename.substring(filename.lastIndexOf('.'));
 		val contentType = getContentType(ext);
@@ -57,6 +69,9 @@ class AWSManager(accessKey: String, secretKey: String, bucket: String) {
 	}
 
 	private def sign(str: String) = {
-		str;
+		val hmac = Mac.getInstance("HmacSHA1");
+		hmac.init(new SecretKeySpec(secretKey.getBytes("utf-8"), "HmacSHA1"));
+		new String(Base64.encodeBase64(hmac.doFinal(str.getBytes("utf-8"))))
+			.replaceAll("¥n", "");
 	}
 }
